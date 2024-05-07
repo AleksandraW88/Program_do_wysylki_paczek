0e7a02ff81e23efe6ca5bce5f38fcdbd746c5b25
Aleksandra <aleksandra.wicek@gmail.com>
2024-05-07 21:08:20 +0200

    Dodano 4 save prigramu

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ najwieksza_liczba_pustych_kg_w_paczce = 0
 numer_paczki_z_najwieksza_iloscia_pustych_kg = None
 
 ilosc_produktow = int(input("Podaj liczbe produktow do wyslania: "))
-print("Podaj wage kazdego z produktow w kilogramach. Pamietaj, ze waga musi byc wieksza od 0 kg i nie wieksza niz 10 kg. ")
+print("Podaj wage kazdego z produktow w kilogramach. Waga musi byc wieksza od 0 kg i nie moze byc wieksza niz 10 kg.")
 for idx in range (ilosc_produktow):
     waga_produktu = float(input(f"Waga produktu {idx + 1}: "));
     if waga_produktu <= 0 or waga_produktu > 10:
-        print("Waga produktu musi byc wieksza od 0 kg, ale nie wieksza niz 10 kg. Program przerwany z powodu podania blednych danych.")
+        print("Waga produktu musi byc wieksza od 0 kg, ale nie wieksza niz 10 kg. Program przerwany. Podano bledne dane.")
         break
     suma_wag += waga_produktu
     print(f"Suma wag wszystkich produktow do wyslania: {suma_wag} kg")
@@ -40,5 +40,5 @@ for idx in range (ilosc_produktow):
         aktualna_waga_paczki += waga_produktu
 if aktualna_waga_paczki > 0:
     liczba_paczek_wyslanych += 1
-print(f"\nLiczba paczek potrzebnych do wysłania: {liczba_paczek_wyslanych}")
+print(f"\nLiczba wyslanych paczek: {liczba_paczek_wyslanych}")
 
