3d2677cdb8602a58eb96f7590130b8dc08100bfa
Aleksandra <aleksandra.wicek@gmail.com>
2024-05-12 13:48:09 +0200

    Dodany kod po probach nieudanych wyliczenia paczki z max iloscia pustych kg

--- main.py
+++ main.py
@@ -13,17 +13,12 @@ MAKSYMALNA_LICZBA_KG_W_PACZCE = 20
 ilosc_produktow =0
 waga_produktu = 0
 suma_wag = 0
-liczba_produktow_w_paczce = 0
-liczba_kg_w_wyslanej_paczce = 0
 liczba_paczek_wyslanych = 0
 aktualna_waga_paczki = 0
-waga_wyslanej_paczki = 0
-Nowa_Paczka = 0
-najciezsza_paczka = 0
-suma_pustych_kg_w_paczce = 0
+suma_pustych_kg_w_paczkach = 0
 najwiecej_pustych_kg = 0
-numer_paczki_z_najwieksza_iloscia_pustych_kg = None
-puste_kg = 0
+nr_paczki_z_max_pustych_kg = None
+max_puste_kg = 0
 
 ilosc_produktow = int(input("Podaj liczbe produktow do wyslania: "))
 print("Podaj wage kazdego z produktow w kilogramach. Waga musi byc wieksza od 0 kg i nie moze byc wieksza niz 10 kg.")
@@ -33,9 +28,11 @@ for idx in range (ilosc_produktow):
         print("Waga produktu musi byc wieksza od 0 kg, ale nie wieksza niz 10 kg. Program przerwany. Podano bledne dane.")
         break
     suma_wag += waga_produktu
-    print(f"Suma wag wszystkich produktow do wyslania: {suma_wag} kg")
+
+    #print(f"Suma wag wszystkich produktow do wyslania: {suma_wag} kg")
     #nie wiem jak i gdzie dać sume wag produktów by nie powtarzala sie za kazdym razem po podaniu wagi kolejnego produktu
     # i tak by nie zaburzyc ponizszej petli if...
+
     if aktualna_waga_paczki + waga_produktu > MAKSYMALNA_LICZBA_KG_W_PACZCE:
         liczba_paczek_wyslanych += 1
         aktualna_waga_paczki = waga_produktu
@@ -43,19 +40,18 @@ for idx in range (ilosc_produktow):
         aktualna_waga_paczki += waga_produktu
 if aktualna_waga_paczki > 0:
     liczba_paczek_wyslanych += 1
-if aktualna_waga_paczki > suma_pustych_kg_w_paczce:
-    suma_pustych_kg_w_paczce = liczba_paczek_wyslanych * MAKSYMALNA_LICZBA_KG_W_PACZCE - suma_wag
-if puste_kg > najwiecej_pustych_kg:
-    najwiecej_pustych_kg = puste_kg
-    numer_paczki_z_najwieksza_iloscia_pustych_kg = liczba_paczek_wyslanych + 1
+if aktualna_waga_paczki > suma_pustych_kg_w_paczkach:
+    suma_pustych_kg_w_paczkach = liczba_paczek_wyslanych * MAKSYMALNA_LICZBA_KG_W_PACZCE - suma_wag
 
+#nie wiem gdzie i jak opisac paczke z najwieksza liczba pustych kg...
+#max_puste_kg = MAKSYMALNA_LICZBA_KG_W_PACZCE - najwiecej_pustych_kg
 
 print("+"*70)
 print("\nPODSUMOWANIE: ")
 print(f"\nLiczba wyslanych paczek: {liczba_paczek_wyslanych}")
 print(f"Liczba wyslanych kilogramow: {suma_wag}")
-print(f"Suma putych kilogramow: {suma_pustych_kg_w_paczce}")
-print(f"Paczka numer {numer_paczki_z_najwieksza_iloscia_pustych_kg} ma najwiecej pustych kilogramow w ilosci: {puste_kg}")
+print(f"Suma putych kilogramow: {suma_pustych_kg_w_paczkach}")
+print(f"Paczka numer {nr_paczki_z_max_pustych_kg} ma najwiecej pustych kilogramow w ilosci: {max_puste_kg}")
 print("+"*70)
 
 
